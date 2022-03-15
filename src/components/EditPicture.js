de4e869845036af991f46f87359bf4c7dc3744b6
veronika <veronika.pilipenko@gmail.com>
2022-03-15 19:29:42 -0400

    added tailwind css

--- src/components/EditPicture.js
+++ src/components/EditPicture.js
@@ -1,9 +1,30 @@
 import React, { useState } from 'react';
 
 const EditPicture = () => {
+
+    const [backgroundColor, setBackgroundColor] = useState('bg-teal-50')
     
+    const handleBackgroundColorChange = (event) => {
+        setBackgroundColor(event.target.value)
+    }
+
     return (
         <div className="outer-container">
+
+            <div className={`h-screen ${backgroundColor}`}>
+                Choose a background color:
+                <br/>
+                <select 
+                value={backgroundColor}
+                onChange={handleBackgroundColorChange} >
+        
+                <option value="bg-teal-50">Color A</option>
+                <option value="bg-indigo-200">Color B</option>
+                <option value="bg-purple-100">Color C</option>
+                </select>
+            
+            <br/><br/>
+        
            <form className="form-container">
                 <input
                     type="text"
@@ -12,7 +33,7 @@ const EditPicture = () => {
 
            <form className="form-container">
                 <input
-                    type="text"
+                    type="color"
                     placeholder="Select your color" />
            </form>
 
@@ -21,6 +42,11 @@ const EditPicture = () => {
                     type="text"
                     placeholder="Enter text" />
            </form>
+
+           <button className="submit-button"
+                    type="submit">
+                        Submit</button>
+            </div>
         </div>
     )
 }
