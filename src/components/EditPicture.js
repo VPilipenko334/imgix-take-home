4aaca7c15eb8610551dd9d0baeff5391212dd3e1
veronika <veronika.pilipenko@gmail.com>
2022-03-15 20:22:32 -0400

    adding more details

--- src/components/EditPicture.js
+++ src/components/EditPicture.js
@@ -16,56 +16,65 @@ const EditPicture = () => {
         setBackgroundColor(event.target.value)
     }
 
+    const handleSubmit = (event) => {
+
+    }
+
     return (
         <div className="outer-wrapper">
             <div className="outer-container">
 
-                <div className={`h-screen ${backgroundColor}`}>
-                    <h1>Welcome to our form:</h1>
+                    <div className={`h-screen ${backgroundColor}`} >
+                        <div className="inner-container">
+                        <h1>Welcome to our form:</h1>
+                        <br/>
 
-                    Please fill out all of the information below and you should 
-                    see immediate changes to your pictures on the right!     
-                    <br/>
-                    <br/>
+                        <p id="description">
+                        Please fill out all of the information below and you should 
+                        see immediate changes to your pictures on the right!     
+                        </p>
 
-                    Choose a background color:
-                    <br/>
-                    <select 
-                    value={backgroundColor}
-                    onChange={handleBackgroundColorChange} >
-            
-                    <option value="bg-teal-50">Color A</option>
-                    <option value="bg-indigo-200">Color B</option>
-                    <option value="bg-purple-100">Color C</option>
-                    </select>
+                        <br/>
+
+                        Choose a background color:
+                        <br/>
+                        <select 
+                        value={backgroundColor}
+                        onChange={handleBackgroundColorChange} >
                 
-                <br/><br/>
-            
-            <form className="form-container">
-                    <input
-                        type="text"
-                        placeholder="Enter your Image URL" />
-            </form>
+                        <option value="bg-teal-50">Color A</option>
+                        <option value="bg-indigo-200">Color B</option>
+                        <option value="bg-purple-100">Color C</option>
+                        </select>
+                    
+                    <br/><br/>
+                
+                <form className="form-container">
+                        <input
+                            type="text"
+                            placeholder="Enter your Image URL" />
+                </form>
 
-            <form className="form-container">
-                    <input
-                        type="color"
-                        placeholder="Select your color" />
-            </form>
+                <form className="form-container">
+                        <input
+                            type="color"
+                            placeholder="Select your color" />
+                </form>
 
-            <form className="form-container">
-                    <input
-                        type="text"
-                        placeholder="Enter text" />
-            </form>
+                <form className="form-container">
+                        <input
+                            type="text"
+                            placeholder="Enter text" />
+                </form>
 
-            <div className="image-preview">
-                <img src={imagePreview} />
-            </div>
+                <div className="image-preview">
+                    <img src={imagePreview} />
+                </div>
 
-            <button className="submit-button"
-                        type="submit">
-                            Submit</button>
+                <button className="submit-button"
+                            type="submit">
+                                Submit</button>
+                    </div>
                 </div>
             </div>
         </div>
