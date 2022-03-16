319d865e28a0cd1acdd3d9efb96bca826a8b8dbb
veronika <veronika.pilipenko@gmail.com>
2022-03-15 23:04:39 -0400

    it WORKS

--- src/components/EditPicture.js
+++ src/components/EditPicture.js
@@ -4,27 +4,41 @@ const EditPicture = () => {
 
     const data = {
         ImageUrl: "",
-        ImageColor: "",
+        ImageColor: "#FFFFFF",
         ImageText: ""
     };
 
     const [backgroundColor, setBackgroundColor] = useState('bg-teal-50')
     const [formInput, setFormInput] = useState(data)
-    const [imagePreview, setImagePreview] = useState('')
-    
+    const [imagePreview, setImagePreview] = useState(`https://assets.imgix.net/examples/butterfly.jpg?txt=${formInput.ImageText}&blend=${formInput.ImageColor.substring(1)}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
+
     const handleBackgroundColorChange = (event) => {
         setBackgroundColor(event.target.value)
     }
 
-    const handleSubmit = (event) => {
+    const updateColor = (event) => {
+            setFormInput({
+                ...formInput,
+                ImageColor: event.target.value
+            })
+            // console.log(formInput.ImageColor.substring(1))
+            setImagePreview(`https://assets.imgix.net/examples/butterfly.jpg?txt=${formInput.ImageText}&blend=${event.target.value.substring(1)}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
+    }
 
+    const updateText = (event) => {
+        setFormInput({
+            ...formInput,
+            ImageText: event.target.value
+        })
+        console.log(formInput)
+        setImagePreview(`https://assets.imgix.net/examples/butterfly.jpg?txt=${event.target.value}&blend=${formInput.ImageColor.substring(1)}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
     }
 
     return (
         <div className="outer-wrapper">
             <div className="outer-container">
-
-                    <div className={`h-screen ${backgroundColor}`} >
+                    <div className={`h-screen ${backgroundColor}`} id="screen" >
+                        <div className="inner-left">
                         <div className="inner-container">
                         <h1>Welcome to our form:</h1>
                         <br/>
@@ -49,32 +63,43 @@ const EditPicture = () => {
                     
                     <br/><br/>
                 
-                <form className="form-container">
+                {/* <form className="form-container">
                         <input
                             type="text"
-                            placeholder="Enter your Image URL" />
-                </form>
+                            placeholder="Enter your Image URL"
+                            value={formInput.ImageUrl}
+                            onChange={updateUrl} />
+                </form> */}
 
                 <form className="form-container">
                         <input
                             type="color"
-                            placeholder="Select your color" />
+                            placeholder="Select your color"
+                            value={formInput.ImageColor}
+                            onChange={updateColor} />
                 </form>
+
 
                 <form className="form-container">
                         <input
                             type="text"
-                            placeholder="Enter text" />
+                            placeholder="Enter text"
+                            value={formInput.ImageText}
+                            onChange={updateText} />
                 </form>
 
-                <div className="image-preview">
-                    <img src={imagePreview} />
-                </div>
-
                 <button className="submit-button"
-                            type="submit">
-                                Submit</button>
+                            type="submit">Submit</button>
+                </div>
+               
                     </div>
+
+                <div className="inner-right">
+                    <div className="image-preview">
+                        <img src={imagePreview} />
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
