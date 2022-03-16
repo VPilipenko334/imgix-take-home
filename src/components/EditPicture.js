7c23e5f4bf492efeb8075dd74a97185b3cc8857f
veronika <veronika.pilipenko@gmail.com>
2022-03-15 23:26:23 -0400

    functionalities work

--- src/components/EditPicture.js
+++ src/components/EditPicture.js
@@ -3,17 +3,25 @@ import React, { useState } from 'react';
 const EditPicture = () => {
 
     const data = {
-        ImageUrl: "",
-        ImageColor: "#FFFFFF",
+        ImageUrl: "examples/butterfly",
+        ImageColor: "#000000",
         ImageText: ""
     };
 
     const [backgroundColor, setBackgroundColor] = useState('bg-teal-50')
     const [formInput, setFormInput] = useState(data)
-    const [imagePreview, setImagePreview] = useState(`https://assets.imgix.net/examples/butterfly.jpg?txt=${formInput.ImageText}&blend=${formInput.ImageColor.substring(1)}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
+    const [imagePreview, setImagePreview] = useState(`https://assets.imgix.net/${formInput.ImageUrl}.jpg?txt=${formInput.ImageText}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
 
     const handleBackgroundColorChange = (event) => {
         setBackgroundColor(event.target.value)
+    }
+
+    const updateUrl = (event) => {
+        setFormInput({
+            ...formInput, 
+            ImageUrl: event.target.value
+        })
+        setImagePreview(`https://assets.imgix.net/${event.target.value}.jpg?txt=${formInput.ImageText}&blend=${formInput.ImageColor.substring(1)}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
     }
 
     const updateColor = (event) => {
@@ -22,7 +30,7 @@ const EditPicture = () => {
                 ImageColor: event.target.value
             })
             // console.log(formInput.ImageColor.substring(1))
-            setImagePreview(`https://assets.imgix.net/examples/butterfly.jpg?txt=${formInput.ImageText}&blend=${event.target.value.substring(1)}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
+            setImagePreview(`https://assets.imgix.net/${formInput.ImageUrl}.jpg?txt=${formInput.ImageText}&blend=${event.target.value.substring(1)}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
     }
 
     const updateText = (event) => {
@@ -30,46 +38,51 @@ const EditPicture = () => {
             ...formInput,
             ImageText: event.target.value
         })
-        console.log(formInput)
-        setImagePreview(`https://assets.imgix.net/examples/butterfly.jpg?txt=${event.target.value}&blend=${formInput.ImageColor.substring(1)}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
+        // console.log(formInput)
+        setImagePreview(`https://assets.imgix.net/${formInput.ImageUrl}.jpg?txt=${event.target.value}&blend=${formInput.ImageColor.substring(1)}&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50`)
     }
+
 
     return (
         <div className="outer-wrapper">
             <div className="outer-container">
-                    <div className={`h-screen ${backgroundColor}`} id="screen" >
-                        <div className="inner-left">
+                 <div className={`h-screen ${backgroundColor}`} id="screen" >
+                    <div className="inner-left">
                         <div className="inner-container">
-                        <h1>Welcome to our form:</h1>
-                        <br/>
+                            <h1>Welcome to our form:</h1>
+                            <br/>
 
-                        <p id="description">
-                        Please fill out all of the information below and you should 
-                        see immediate changes to your pictures on the right!     
-                        </p>
+                            <p id="description">
+                            Please fill out all of the information below and you should 
+                            see immediate changes to your pictures on the right!     
+                            </p>
 
-                        <br/>
+                            <br/>
 
-                        Choose a background color:
-                        <br/>
-                        <select 
-                        value={backgroundColor}
-                        onChange={handleBackgroundColorChange} >
-                
-                        <option value="bg-teal-50">Color A</option>
-                        <option value="bg-indigo-200">Color B</option>
-                        <option value="bg-purple-100">Color C</option>
-                        </select>
+                            Choose a background color:
+                            <br/>
+                            <select 
+                            value={backgroundColor}
+                            onChange={handleBackgroundColorChange} >
                     
+                            <option value="bg-teal-50">Color A</option>
+                            <option value="bg-indigo-200">Color B</option>
+                            <option value="bg-purple-100">Color C</option>
+                            </select>
                     <br/><br/>
                 
-                {/* <form className="form-container">
-                        <input
-                            type="text"
-                            placeholder="Enter your Image URL"
-                            value={formInput.ImageUrl}
-                            onChange={updateUrl} />
-                </form> */}
+                <select
+                value={formInput.ImageUrl}
+                onChange={updateUrl}
+                >
+                    <option value="examples/butterfly">Butterfly</option>
+                    <option value="unsplash/bridge">Bridge</option>
+                    <option value="unsplash/pineneedles">Pine Needles</option>
+                    <option value="unsplash/motorbike">Motorbike</option>
+                    <option value="unsplash/mountains">Mountains</option>
+                </select>
+                <br/>
+                <br/>
 
                 <form className="form-container">
                         <input
@@ -88,11 +101,9 @@ const EditPicture = () => {
                             onChange={updateText} />
                 </form>
 
-                <button className="submit-button"
-                            type="submit">Submit</button>
                 </div>
                
-                    </div>
+                </div>
 
                 <div className="inner-right">
                     <div className="image-preview">
